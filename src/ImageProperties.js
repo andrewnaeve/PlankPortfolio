ee7e64204c3b51f3e03cbd94ef5c67c23d2bd49a
Andrew Naeve <andrewnaeve@Andrews-MacBook-Pro.local>
2017-09-16 14:16:15 -0700

    Sanitized names

--- src/ImageProperties.js
+++ src/ImageProperties.js
@@ -1,5 +1,5 @@
-import Holly from './img/Holly.jpg';
-import Arden from './img/Arden.jpg';
+import HollyAndSebastian from './img/Holly and Sebastian.jpg';
+import ArdenInRome from './img/Arden In Rome.jpg';
 import Vase from './img/Vase.jpg';
 import OrangeFlowers from './img/Flowers.jpg';
 import Lavendar from './img/Lavendar.jpg';
@@ -8,13 +8,13 @@ import BlackRoses from './img/Roses with Black Background.jpg';
 import Mennonites from './img/Mennonites.jpg';
 import BlueFloral from './img/Blue Floral.jpg';
 import TaupeFlowers from './img/Taupe.jpg';
-import ElBirdo from './img/El Birdo.jpg';
-import Blueberries from './img/Blueberries.jpg';
+import WhatMeWorry from './img/What, Me Worry.jpg';
+import Berries from './img/Berries at the Huntington.jpg';
 
 const ImageProperties = [
 	{
 		title: 'Holly and Sebastian',
-		url: Holly,
+		url: HollyAndSebastian,
 		description: '24" x 20," oil on canvas'
 	},
 	{
@@ -29,7 +29,7 @@ const ImageProperties = [
 	},
 	{
 		title: 'What, Me Worry?',
-		url: ElBirdo,
+		url: WhatMeWorry,
 		description: '60" x 70," oil on canvas'
 	},
 	{
@@ -49,7 +49,7 @@ const ImageProperties = [
 	},
 	{
 		title: 'Arden in Rome',
-		url: Arden,
+		url: ArdenInRome,
 		description: '6" x 4," oil on canvas'
 	},
 	{
@@ -59,7 +59,7 @@ const ImageProperties = [
 	},
 	{
 		title: 'Berries at the Huntington',
-		url: Blueberries,
+		url: Berries,
 		description: '60" x 70," oil on canvas'
 	},
 	{
