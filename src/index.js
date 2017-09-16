956c3cd4b07ced94daa98d2490bb22396c03c671
Andrew Naeve <andrewnaeve@Andrews-MacBook-Pro.local>
2017-09-15 23:18:13 -0700

    removed all css stylesheets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
 import App from './App.js';
 import { BrowserRouter } from 'react-router-dom';
 // import registerServiceWorker from './registerServiceWorker';
@@ -8,6 +7,23 @@ import { unregister } from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
 import ScrollToTop from './components/Utilities/ScrollToTop';
+import { injectGlobal } from 'styled-components';
+import Abel from './fonts/Abel-Regular.ttf';
+import FontAwesome from './fonts/fontawesome-webfont.woff2?v=4.7.0';
+
+injectGlobal`
+	@font-face {
+		font-family: 'Abel';
+		src: url('${Abel}');
+		format('ttf');
+	}
+	@font-face {
+		font-family: 'FontAwesome';
+		src: url('${FontAwesome}');
+		format('woff2');
+	}
+`;
+
 render(
 	<Provider store={store}>
 		<BrowserRouter>
