567a3bc595c5519a99ee1e23ffc0749342d40e0f
Andrew Naeve <andrewnaeve@Andrews-MacBook-Pro.local>
2017-09-17 20:56:35 -0700

    remove files

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,6 +1,5 @@
 import { injectGlobal } from 'styled-components';
 import Abel from './fonts/Abel-Regular.ttf';
-import FontAwesome from './fonts/fontawesome-webfont.woff2?v=4.7.0';
 
 injectGlobal`
 	@font-face {
@@ -8,33 +7,12 @@ injectGlobal`
 		src: url('${Abel}');
 		format('ttf');
 	}
-	@font-face {
-		font-family: 'FontAwesome';
-		src: url('${FontAwesome}');
-		format('woff2');
-	}
-    .fa {
-		display: inline-block;
-		font: normal normal normal 14px/1 FontAwesome;
-		font-size: inherit;
-		text-rendering: auto;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-	  }
   
-	  .fa-instagram:before {
-		content: '\f16d';
-	  }
-  
-	  .fa-envelope-o:before {
-		content: '\f003';
-	  }
-  
-	  .iframeStyle {
+	.iframeStyle {
 		position: absolute;
 		top: 0;
 		left: 0;
 		width: 100%;
 		height: 100%;
-	  }
+	}
 `;
