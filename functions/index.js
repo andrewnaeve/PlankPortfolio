bde2e1113a4ab33f5252049044395b2d56de5459
andrewnaeve <andrewnaeve@gmail.com>
2018-09-29 16:32:56 -0700

    add firebase setup

--- functions/index.js
+++ functions/index.js
@@ -2,3 +2,6 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
+const firestore = firebase.firestore();
+const settings = { /* your settings... */ timestampsInSnapshots: true };
+firestore.settings(settings);
