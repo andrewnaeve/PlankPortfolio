f4e45f607270ce5d347181ef50b31ab9e7ec5a56
andrewnaeve <andrewnaeve@gmail.com>
2018-09-29 16:45:00 -0700

    cleanup

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,3 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-const firestore = firebase.firestore();
-const settings = { /* your settings... */ timestampsInSnapshots: true };
-firestore.settings(settings);
