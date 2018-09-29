7f3dc4ffa8f6ef5e7024ec26d8fb8500a0841ad5
andrewnaeve <andrewnaeve@gmail.com>
2018-09-29 16:30:59 -0700

    upgrade fns

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,3 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((req, res) => {
-	res.send('Hello from Firebase!');
-});
-
-exports.paymentReq = functions.https.onRequest((req, res) => {
-	console.log('req', req);
-	console.log('req.query', req.query);
-	console.log('req.query.custo', req.query.customerInfo);
-	console.log('reqparams', req.params);
-	res.send('hi');
-});
